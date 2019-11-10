e03f36481c6842bb69b588499180d67e6559a702
oli <shubham@tripoto.com>
2019-11-10 23:13:33 +0530

    Added tasks to install kops and kubectl and pull git repo (iac)

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,6 +37,13 @@ resource "aws_security_group" "ssh_external" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    egress {
+      from_port       = 0
+      to_port         = 0
+      protocol        = "-1"
+      cidr_blocks     = ["0.0.0.0/0"]
+    }
+
     vpc_id = "${aws_vpc.default.id}"
 
     tags = {
