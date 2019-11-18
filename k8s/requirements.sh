719b4bccad6af6e99da85e717cef1c385fedd131
oli <shubham@tripoto.com>
2019-11-18 22:25:23 +0530

    Added helm install for nginx-ingress

--- k8s/requirements.sh
+++ k8s/requirements.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+helm install stable/nginx-ingress --set service.type=LoadBalancer --namespace ingress-nginx
 helm install stable/prometheus --namespace monitoring-prod --generate-name
 helm install stable/grafana -f monitoring/values.yml --namespace monitoring-prod --generate-name
 
